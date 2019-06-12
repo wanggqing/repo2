84973d9f0d4db83aa0e3e4988bdf3bc423db0f48
wanggqing <wanggqing@126.com>
2019-06-12 22:46:01 +0800

    commit IUser

---
+++ src/main/java/cn/itcast/User.java
@@ -0,0 +1,42 @@
+package cn.itcast;
+
+public class User {
+    private String name;
+    private Integer age;
+    private String address;
+
+    public String getName() {
+        return name;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public User() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Git...");
+    }
+    public void method() {
+
+    }
+}
